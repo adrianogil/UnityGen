c498711d795d481d1fb31c9c645e83dd6bcce410
Adriano Gil <adrianogil.san@gmail.com>
2017-03-24 09:33:50 -0400

    Generate unity project using unity version from a specific path

--- src/unity_project_gen.sh
+++ src/unity_project_gen.sh
@@ -1,19 +1,18 @@
 # Create a unity project from a project name
+# Usage:
+# ./unity_project_gen.sh PROJECT_NAME
+# ./unity_project_gen.sh UNITY_VERSION PROJECT_NAME
 
-unity_5_5=/Applications/Unity5.5/Unity5.5.app/Contents/MacOS/Unity
-unity_5_3_6=/Applications/Unity5.3.6/Unity5.3.6.app/Contents/MacOS/Unity
-unity_5_3_5=/Applications/Unity5.3.5/Unity5.3.5.app/Contents/MacOS/Unity
-unity=$unity_5_5
-
-if [ -z "$1" ]
+if [ -z "$2" ]
 then
-    repo='local'
+    project_name=$1
+    unity_version='5_6_0_11b'
 else
     unity_version=$1
-    unity='/Applications/Unity'$unity_version'/Unity'$unity_version'.app/Contents/MacOS/Unity'
+    project_name=$2
 fi
 
-project_name=$2
+unity="/Applications/Unity"${unity_version}"/Unity"${unity_version}".app/Contents/MacOS/Unity"
 
 $unity -createProject ${project_name} -batchmode -nographics -quit
 
