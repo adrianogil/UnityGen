a9d5092804db674d9c09f845ef023d9a896ca470
Adriano Gil <adrianogil.san@gmail.com>
2017-10-04 15:20:33 -0400

    Added unity version as argument

--- src/unity_project_gen.sh
+++ src/unity_project_gen.sh
@@ -5,7 +5,15 @@ unity_5_3_6=/Applications/Unity5.3.6/Unity5.3.6.app/Contents/MacOS/Unity
 unity_5_3_5=/Applications/Unity5.3.5/Unity5.3.5.app/Contents/MacOS/Unity
 unity=$unity_5_5
 
-project_name=$1
+if [ -z "$1" ]
+then
+    repo='local'
+else
+    unity_version=$1
+    unity='/Applications/Unity'$unity_version'/Unity'$unity_version'.app/Contents/MacOS/Unity'
+fi
+
+project_name=$2
 
 $unity -createProject ${project_name} -batchmode -nographics -quit
 
