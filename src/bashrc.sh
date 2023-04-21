6d08632d96d900d6ba2844104728ee172d178a8c
Adriano Gil <adrianogil.san@gmail.com>
2023-04-21 09:37:08 -0400

    Fixed gen-unity-project command

--- src/bashrc.sh
+++ src/bashrc.sh
@@ -1,5 +1,44 @@
-alias gen-unity-project=${UNITY_GEN_PATH}'/unity_project_gen.sh'
 alias gen-android-unity-plugin=${UNITY_GEN_PATH}'/unity_android_plugin_gen.sh'
+
+function gen-unity-project()
+{
+    # Create a unity project from a project name
+    # Usage:
+    # ./unity_project_gen.sh PROJECT_NAME
+    # ./unity_project_gen.sh UNITY_VERSION PROJECT_NAME
+
+    if [ -z "$2" ]
+    then
+        project_name=$1
+        unity_version=$(ls ${UNITY_HUB_APPS_DIR} | default-fuzzy-finder)
+    else
+        unity_version=$1
+        project_name=$2
+    fi
+
+    if [ -z "$UNITY_APPS_FOLDER" ]
+    then
+        unity_main_path=/Applications/
+    else
+        unity_main_path=${UNITY_APPS_FOLDER}
+    fi
+
+    unity_exe="${UNITY_HUB_APPS_DIR}/"${unity_version}"/Unity.app/Contents/MacOS/Unity"
+    ${unity_exe} -createProject ${project_name} -batchmode -nographics -quit
+
+    assets_folder=${project_name}/Assets
+
+    # Create default folders
+    echo "Generating default folder structure at Assets folder"
+    mkdir -p ${assets_folder}/Editor
+    mkdir -p ${assets_folder}/Materials
+    mkdir -p ${assets_folder}/Prefabs
+    mkdir -p ${assets_folder}/Resources
+    mkdir -p ${assets_folder}/Scenes
+    mkdir -p ${assets_folder}/Scripts
+    mkdir -p ${assets_folder}/Shaders
+    mkdir -p ${assets_folder}/Textures
+}
 
 _gen-unity-project()
 {
